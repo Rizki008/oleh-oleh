fd8bb7f8db9dafac7138db84b0deb8ca70a129e1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-10 06:07:00 +0700

    menambahkan setting view frontend:

---
+++ application/views/layout/frontend/v_header.php
@@ -0,0 +1,16 @@
+<body>
+
+	<!--PreLoader-->
+	<div class="loader">
+		<div class="loader-inner">
+			<div class="circle"></div>
+		</div>
+	</div>
+	<!--PreLoader Ends-->
+
+	<!-- header -->
+	<div class="top-header-area" id="sticker">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 text-center">
+					<div class="main-menu-wrap">
