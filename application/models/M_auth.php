20f33ad16ac8adf2a31cee471095eff4725b31a8
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-13 04:53:43 +0700

    menambahkan authentifikasi login

---
+++ application/models/M_auth.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_auth extends CI_Model
+{
+	public function user_login($username, $password)
+	{
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->where(array('username' => $username, 'password' => $password));
+		return $this->db->get()->row();
+	}
+
+	public function pelanggan_login($email, $password)
+	{
+		$this->db->select('*');
+		$this->db->from('pelanggan');
+		$this->db->where(array('email' => $email, 'password' => $password));
+		return $this->db->get()->row();
+	}
+}
