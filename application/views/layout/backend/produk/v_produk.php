8c038620a79cf6ada5aa6232447b2b4317884ade
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-14 09:37:58 +0700

    mengubah tampilan backend produk

--- application/views/layout/backend/produk/v_produk.php
+++ application/views/layout/backend/produk/v_produk.php
@@ -24,6 +24,7 @@
 									<th> Nama Kategori </th>
 									<th> Berat </th>
 									<th> Harga </th>
+									<th> Nama Diskon </th>
 									<th> Diskon </th>
 									<th> Stock </th>
 									<th> Deskripsi </th>
@@ -39,8 +40,9 @@
 										<td> <?= $value->nama_produk ?> </td>
 										<td> <?= $value->nama_kategori ?> </td>
 										<td> <?= $value->berat ?> <?= $value->product_unit ?> </td>
-										<td> <?= $value->harga ?> </td>
-										<td> <?= $value->diskon ?> </td>
+										<td> <?= number_format($value->harga, 0) ?> </td>
+										<td> <?= $value->nama_diskon ?> </td>
+										<td> Rp. <?= number_format($value->diskon, 0) ?> </td>
 										<td><?= $value->stock ?>
 											<?php if ($value->stock <= 10) { ?>
 												<div class="progress">
@@ -59,6 +61,7 @@
 										<td> <?= $value->deskripsi ?> </td>
 										<td> <a href="<?= base_url('produk/update/' . $value->id_produk) ?>" class="btn btn-warning">Update</a>
 											<button class="btn btn-danger" data-toggle="modal" data-target="#delete<?= $value->id_produk ?>">Delete</button>
+											<button class="btn btn-info" data-toggle="modal" data-target="#diskon<?= $value->id_produk ?>">Diskon</button>
 										</td>
 									</tr>
 								<?php } ?>
@@ -92,3 +95,32 @@
 			</div>
 		</div>
 	<?php } ?>
+	<!-- /.modal Delete -->
+	<div class="modal fade" id="diskon<?= $value->id_produk ?>">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Diskon <?= $value->nama_produk ?></h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<?php echo form_open('produk/diskon/' . $value->id_produk) ?>
+				<div class="modal-body">
+					<div class="from-group">
+						<label for="">Nama Diskon</label>
+						<input type="text" name="nama_diskon" class="form-control" required>
+					</div>
+					<div class="from-group">
+						<label for="">Harga Diskon</label>
+						<input type="text" name="diskon" class="form-control" required>
+					</div>
+				</div>
+				<div class="modal-footer justify-content-between">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Update</button>
+				</div>
+				<?php echo form_close() ?>
+			</div>
+		</div>
+	</div>
