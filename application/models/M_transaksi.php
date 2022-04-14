07ae4a9bf87b29ae624406e515be306dd059b9a2
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-14 08:20:45 +0700

    menambahkan model transaksi

--- application/models/M_transaksi.php
+++ application/models/M_transaksi.php
@@ -14,4 +14,67 @@ class M_transaksi extends CI_Model
 	{
 		$this->db->insert('rinci_transaksi', $data_rinci);
 	}
+
+	public function upload_buktibayar($data)
+	{
+		$this->db->where('id_transaksi', $data['id_transaksi']);
+		$this->db->update('transaksi', $data);
+	}
+
+	public function belum_bayar()
+	{
+		$this->db->select('*');
+		$this->db->from('transaksi');
+		$this->db->where('status_order=0');
+		$this->db->order_by('id_transaksi', 'desc');
+		return $this->db->get()->result();
+	}
+	public function diproses()
+	{
+		$this->db->select('*');
+		$this->db->from('transaksi');
+		$this->db->where('status_order=1');
+		$this->db->order_by('id_transaksi', 'desc');
+		return $this->db->get()->result();
+	}
+	public function dikirim()
+	{
+		$this->db->select('*');
+		$this->db->from('transaksi');
+		$this->db->where('status_order=2');
+		$this->db->order_by('id_transaksi', 'desc');
+		return $this->db->get()->result();
+	}
+	public function selesai()
+	{
+		$this->db->select('*');
+		$this->db->from('transaksi');
+		$this->db->where('status_order=3');
+		$this->db->order_by('id_transaksi', 'desc');
+		return $this->db->get()->result();
+	}
+
+	public function detail_pesanan($id_transaksi)
+	{
+		$this->db->select('*');
+		$this->db->from('transaksi');
+		$this->db->where('id_transaksi', $id_transaksi);
+		return $this->db->get()->row();
+	}
+
+	public function pesanan_detail($no_order)
+	{
+		$this->db->select('*');
+		$this->db->from('transaksi');
+		$this->db->join('rinci_transaksi', 'transaksi.no_order = rinci_transaksi.no_order', 'left');
+		$this->db->join('produk', 'rinci_transaksi.id_produk = produk.id_produk', 'left');
+		$this->db->where('transaksi.no_order', $no_order);
+		return $this->db->get()->result();
+	}
+	public function rekening()
+	{
+		$this->db->select('*');
+		$this->db->from('rekening');
+		return $this->db->get()->result();
+	}
 }
