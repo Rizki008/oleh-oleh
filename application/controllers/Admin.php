35f67a1ce49038c131f515a441d05e348814bf2f
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-17 09:07:57 +0700

    menambahkan proses lokasi

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,6 +4,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Admin extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_admin');
+	}
 
 	public function index()
 	{
@@ -12,5 +17,31 @@ class Admin extends CI_Controller
 			'isi' => 'v_admin'
 		);
 		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
+	}
+
+	public function lokasi()
+	{
+		$this->form_validation->set_rules('nama_toko', 'Nama Toko', 'required', array('required' => '%s Mohon Untuk Diisi'));
+		$this->form_validation->set_rules('kota', 'Kota', 'required', array('required' => '%s Mohon Untuk Diisi'));
+		$this->form_validation->set_rules('alamat', 'Alamat', 'required', array('required' => '%s Mohon Untuk Diisi'));
+
+		if ($this->form_validation->run() == FALSE) {
+			$data = array(
+				'title' => 'Setting',
+				'lokasi' => $this->m_admin->data_lokasi(),
+				'isi' => 'layout/backend/lokasi/v_lokasi_toko'
+			);
+			$this->load->view('layout/backend/v_wrapper', $data, FALSE);
+		} else {
+			$data = array(
+				'id' => 1,
+				'lokasi' => $this->input->post('kota'),
+				'nama_toko' => $this->input->post('nama_toko'),
+				'alamat' => $this->input->post('alamat'),
+			);
+			$this->m_admin->update($data);
+			$this->session->set_flashdata('pesan', 'Lokasi Toko berhasil di update');
+			redirect('admin/lokasi');
+		}
 	}
 }
