3e4987d98bd087e57007ec81ae9fc97cf37cb2e9
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-03 15:29:32 +0700

    megambil data login pemilik

--- application/models/M_auth.php
+++ application/models/M_auth.php
@@ -11,6 +11,13 @@ class M_auth extends CI_Model
 		$this->db->where(array('username' => $username, 'password' => $password));
 		return $this->db->get()->row();
 	}
+	public function pemilik_login($username, $password)
+	{
+		$this->db->select('*');
+		$this->db->from('pemilik');
+		$this->db->where(array('username' => $username, 'password' => $password));
+		return $this->db->get()->row();
+	}
 
 	public function pelanggan_login($username, $password)
 	{
