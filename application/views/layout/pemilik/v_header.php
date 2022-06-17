04a07e4c97a845e9cfcddd61ce3a41ae4675c943
rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-17 21:54:30 +0700

    mengubah tampilan header

--- application/views/layout/pemilik/v_header.php
+++ application/views/layout/pemilik/v_header.php
@@ -11,14 +11,6 @@
 					<span class="mdi mdi-menu"></span>
 				</button>
 				<div class="search-field d-none d-xl-block">
-					<!-- <form class="d-flex align-items-center h-100" action="#">
-						<div class="input-group">
-							<div class="input-group-prepend bg-transparent">
-								<i class="input-group-text border-0 mdi mdi-magnify"></i>
-							</div>
-							<input type="text" class="form-control bg-transparent border-0" placeholder="Search products">
-						</div>
-					</form> -->
 				</div>
 				<ul class="navbar-nav navbar-nav-right">
 					<li class="nav-item nav-profile dropdown">
@@ -27,7 +19,7 @@
 								<img src="<?= base_url() ?>backend/assets/images/faces/face28.png" alt="image">
 							</div>
 							<div class="nav-profile-text">
-								<p class="mb-1 text-black">Henry Klein</p>
+								<p class="mb-1 text-black"><?= $this->session->userdata('username') ?></p>
 							</div>
 						</a>
 						<div class="dropdown-menu navbar-dropdown dropdown-menu-right p-0 border-0 font-size-sm" aria-labelledby="profileDropdown" data-x-placement="bottom-end">
@@ -36,30 +28,6 @@
 							</div>
 							<div class="p-2">
 								<h5 class="dropdown-header text-uppercase pl-2 text-dark">User Options</h5>
-								<!-- <a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="#">
-									<span>Inbox</span>
-									<span class="p-0">
-										<span class="badge badge-primary">3</span>
-										<i class="mdi mdi-email-open-outline ml-1"></i>
-									</span>
-								</a>
-								<a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="#">
-									<span>Profile</span>
-									<span class="p-0">
-										<span class="badge badge-success">1</span>
-										<i class="mdi mdi-account-outline ml-1"></i>
-									</span>
-								</a>
-								<a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="javascript:void(0)">
-									<span>Settings</span>
-									<i class="mdi mdi-settings"></i>
-								</a>
-								<div role="separator" class="dropdown-divider"></div>
-								<h5 class="dropdown-header text-uppercase  pl-2 text-dark mt-2">Actions</h5>
-								<a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="#">
-									<span>Lock Account</span>
-									<i class="mdi mdi-lock ml-1"></i>
-								</a> -->
 								<a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="<?= base_url('pemilik/logout_pemilik') ?>">
 									<span>Log Out</span>
 									<i class="mdi mdi-logout ml-1"></i>
@@ -68,92 +36,8 @@
 						</div>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link count-indicator dropdown-toggle" id="messageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-							<i class="mdi mdi-email-outline"></i>
-							<span class="count-symbol bg-success"></span>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
-							<h6 class="p-3 mb-0 bg-primary text-white py-4">Messages</h6>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<img src="<?= base_url() ?>backend/assets/images/faces/face4.jpg" alt="image" class="profile-pic">
-								</div>
-								<div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-									<h6 class="preview-subject ellipsis mb-1 font-weight-normal">Mark send you a message</h6>
-									<p class="text-gray mb-0"> 1 Minutes ago </p>
-								</div>
-							</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<img src="<?= base_url() ?>backend/assets/images/faces/face2.jpg" alt="image" class="profile-pic">
-								</div>
-								<div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-									<h6 class="preview-subject ellipsis mb-1 font-weight-normal">Cregh send you a message</h6>
-									<p class="text-gray mb-0"> 15 Minutes ago </p>
-								</div>
-							</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<img src="<?= base_url() ?>backend/assets/images/faces/face3.jpg" alt="image" class="profile-pic">
-								</div>
-								<div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-									<h6 class="preview-subject ellipsis mb-1 font-weight-normal">Profile picture updated</h6>
-									<p class="text-gray mb-0"> 18 Minutes ago </p>
-								</div>
-							</a>
-							<div class="dropdown-divider"></div>
-							<h6 class="p-3 mb-0 text-center">4 new messages</h6>
-						</div>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-							<i class="mdi mdi-bell-outline"></i>
-							<span class="count-symbol bg-danger"></span>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-							<h6 class="p-3 mb-0 bg-primary text-white py-4">Notifications</h6>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<div class="preview-icon bg-success">
-										<i class="mdi mdi-calendar"></i>
-									</div>
-								</div>
-								<div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-									<h6 class="preview-subject font-weight-normal mb-1">Event today</h6>
-									<p class="text-gray ellipsis mb-0"> Just a reminder that you have an event today </p>
-								</div>
-							</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<div class="preview-icon bg-warning">
-										<i class="mdi mdi-settings"></i>
-									</div>
-								</div>
-								<div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-									<h6 class="preview-subject font-weight-normal mb-1">Settings</h6>
-									<p class="text-gray ellipsis mb-0"> Update dashboard </p>
-								</div>
-							</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<div class="preview-icon bg-info">
-										<i class="mdi mdi-link-variant"></i>
-									</div>
-								</div>
-								<div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-									<h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>
-									<p class="text-gray ellipsis mb-0"> New admin wow! </p>
-								</div>
-							</a>
-							<div class="dropdown-divider"></div>
-							<h6 class="p-3 mb-0 text-center">See all notifications</h6>
-						</div>
 					</li>
 				</ul>
 				<button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
