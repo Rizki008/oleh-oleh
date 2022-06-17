52c5ddec68baf561b2e252df321f4c2bdc4b29de
rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-17 21:55:22 +0700

    menampilkan semua data transaksi

--- application/views/layout/backend/transaksi/v_transaksi.php
+++ application/views/layout/backend/transaksi/v_transaksi.php
@@ -1,11 +1,11 @@
 <div class="main-panel">
 	<div class="content-wrapper">
 		<div class="page-header">
-			<h3 class="page-title"> Basic Tables </h3>
+			<h3 class="page-title"> <?= $title ?> </h3>
 			<nav aria-label="breadcrumb">
 				<ol class="breadcrumb">
 					<li class="breadcrumb-item"><a href="#">Tables</a></li>
-					<li class="breadcrumb-item active" aria-current="page">Basic tables</li>
+					<li class="breadcrumb-item active" aria-current="page"><?= $title ?></li>
 				</ol>
 			</nav>
 		</div>
@@ -13,39 +13,46 @@
 			<div class="col-lg-12 grid-margin stretch-card">
 				<div class="card">
 					<div class="card-body">
-						<h4 class="card-title">Hoverable Table</h4>
-						<p class="card-description"> Add class <code>.table-hover</code>
+						<h4 class="card-title">Data Transaksi</h4>
+						<!-- <p class="card-description"> Add class <code>.table-hover</code> -->
 						</p>
-						<table class="table table-hover">
+						<table class="table table-hover responsive-menu">
 							<thead>
 								<tr>
+									<th>No</th>
 									<th>Nama Pelanggan</th>
 									<th>No Order</th>
 									<th>Tanggal Order</th>
-									<th>Expedisi</th>
-									<th>Estimasi</th>
-									<th>Biaya Pengiriman</th>
-									<th>Total Bayar</th>
-									<th>No Pengiriman (No Resi)</th>
+									<th>Alamat</th>
+									<th>Jasa Pengiriman</th>
+									<th>Berat Produk</th>
+									<th>Rincian Biaya</th>
+									<th>No Resi</th>
 									<th>Action</th>
 								</tr>
 							</thead>
 							<tbody>
-								<?php foreach ($pesanan as $key => $value) { ?>
+								<?php $no = 1;
+								foreach ($pesanan as $key => $value) { ?>
 									<tr>
+										<td><?= $no++ ?></td>
 										<td><?= $value->nama_pelanggan ?></td>
 										<td><?= $value->no_order ?></td>
 										<td><?= $value->tgl_order ?></td>
-										<td><?= $value->expedisi ?> <br> <?= $value->paket ?></td>
-										<td><?= $value->estimasi ?></td>
-										<td class="text-warning">Rp. <?= number_format($value->ongkir, 0) ?><i class="mdi mdi-arrow-down"></i></td>
-										<td>Rp. <?= number_format($value->total_bayar, 0) ?>
-											<?php if ($value->status_bayar == 0) { ?>
+										<td>Provinsi : <?= $value->provinsi ?> <br> Kota : <?= $value->kota ?> <br> Kode Pos : <?= $value->kode_pos ?></td>
+										<td>Expedisi : <?= $value->expedisi ?> <br> <?= $value->paket ?> <br>Estimasi : <?= $value->estimasi ?></td>
+										<td><?= $value->berat ?> Kg</td>
+										<td class="text-warning">Total Belanja : Rp. <?= number_format($value->grand_total, 0) ?>
+											<br>Biaya ongkir : Rp. <?= number_format($value->ongkir, 0) ?>
+											<br>Total Belanja : Rp. <?= number_format($value->total_bayar, 0) ?>
+											<!-- <i class="mdi mdi-arrow-down"></i> -->
+											<br>Status : <?php if ($value->status_bayar == 0) { ?>
 												<label class="badge badge-danger">Belum Bayar</label>
 											<?php } else { ?>
 												<label class="badge badge-warning">Belum Bayar</label>
 											<?php } ?>
 										</td>
+
 										<td></td>
 										<td>
 											<?php if ($value->status_bayar == 1) { ?>
@@ -55,46 +62,61 @@
 										</td>
 									</tr>
 								<?php } ?>
-								<?php foreach ($diproses as $key => $value) { ?>
+								<?php $no = 1;
+								foreach ($diproses as $key => $value) { ?>
 									<tr>
+										<td><?= $no++ ?></td>
 										<td><?= $value->nama_pelanggan ?></td>
 										<td><a href="<?= base_url('transaksi/detail/' . $value->no_order) ?>"><?= $value->no_order ?></a></td>
 										<td><?= $value->tgl_order ?></td>
-										<td><?= $value->expedisi ?> <br> <?= $value->paket ?></td>
-										<td><?= $value->estimasi ?></td>
-										<td class="text-warning">Rp. <?= number_format($value->ongkir, 0) ?><i class="mdi mdi-arrow-down"></i></td>
-										<td>Rp. <?= number_format($value->total_bayar, 0) ?>
-											<label class="badge badge-info">Diproses</label>
+										<td>Provinsi : <?= $value->provinsi ?> <br> Kota : <?= $value->kota ?> <br> Kode Pos : <?= $value->kode_pos ?></td>
+										<td>Expedisi : <?= $value->expedisi ?> <br> <?= $value->paket ?> <br>Estimasi : <?= $value->estimasi ?></td>
+										<td><?= $value->berat ?> Kg</td>
+										<td class="text-warning">Total Belanja : Rp. <?= number_format($value->grand_total, 0) ?>
+											<br>Biaya ongkir : Rp. <?= number_format($value->ongkir, 0) ?>
+											<br>Total Belanja : Rp. <?= number_format($value->total_bayar, 0) ?>
+											<!-- <i class="mdi mdi-arrow-down"></i> -->
+											<br>Status : <label class="badge badge-info">Diproses</label>
 										</td>
 										<td></td>
 										<td></td>
 									</tr>
 								<?php } ?>
-								<?php foreach ($dikirim as $key => $value) { ?>
+								<?php $no = 1;
+								foreach ($dikirim as $key => $value) { ?>
 									<tr>
+										<td><?= $no++ ?></td>
 										<td><?= $value->nama_pelanggan ?></td>
 										<td><?= $value->no_order ?></td>
 										<td><?= $value->tgl_order ?></td>
-										<td><?= $value->expedisi ?> <br> <?= $value->paket ?></td>
-										<td><?= $value->estimasi ?></td>
-										<td class="text-warning">Rp. <?= number_format($value->ongkir, 0) ?><i class="mdi mdi-arrow-down"></i></td>
-										<td>Rp. <?= number_format($value->total_bayar, 0) ?>
-											<label class="badge badge-primary">Dikirim</label>
+										<td>Provinsi : <?= $value->provinsi ?> <br> Kota : <?= $value->kota ?> <br> Kode Pos : <?= $value->kode_pos ?></td>
+										<td>Expedisi : <?= $value->expedisi ?> <br> <?= $value->paket ?> <br>Estimasi : <?= $value->estimasi ?></td>
+										<td><?= $value->berat ?> Kg</td>
+										<td class="text-warning">Total Belanja : Rp. <?= number_format($value->grand_total, 0) ?>
+											<br>Biaya ongkir : Rp. <?= number_format($value->ongkir, 0) ?>
+											<br>Total Belanja : Rp. <?= number_format($value->total_bayar, 0) ?>
+											<!-- <i class="mdi mdi-arrow-down"></i> -->
+											<br>Status : <label class="badge badge-primary">Dikirim</label>
 										</td>
 										<td class="text-info"><?= $value->nama_pengirim ?></td>
 										<td></td>
 									</tr>
 								<?php } ?>
-								<?php foreach ($selesai as $key => $value) { ?>
+								<?php $no = 1;
+								foreach ($selesai as $key => $value) { ?>
 									<tr>
+										<td><?= $no++ ?></td>
 										<td><?= $value->nama_pelanggan ?></td>
 										<td><?= $value->no_order ?></td>
 										<td><?= $value->tgl_order ?></td>
-										<td><?= $value->expedisi ?> <br> <?= $value->paket ?></td>
-										<td><?= $value->estimasi ?></td>
-										<td class="text-warning">Rp. <?= number_format($value->ongkir, 0) ?><i class="mdi mdi-arrow-down"></i></td>
-										<td>Rp. <?= number_format($value->total_bayar, 0) ?>
-											<label class="badge badge-success">Selesai</label>
+										<td>Provinsi : <?= $value->provinsi ?> <br> Kota : <?= $value->kota ?> <br> Kode Pos : <?= $value->kode_pos ?></td>
+										<td>Expedisi : <?= $value->expedisi ?> <br> <?= $value->paket ?> <br>Estimasi : <?= $value->estimasi ?></td>
+										<td><?= $value->berat ?> Kg</td>
+										<td class="text-warning">Total Belanja : Rp. <?= number_format($value->grand_total, 0) ?>
+											<br>Biaya ongkir : Rp. <?= number_format($value->ongkir, 0) ?>
+											<br>Total Belanja : Rp. <?= number_format($value->total_bayar, 0) ?>
+											<!-- <i class="mdi mdi-arrow-down"></i> -->
+											<br>Status : <label class="badge badge-success">Selesai</label>
 										</td>
 										<td class="text-info"><?= $value->nama_pengirim ?></td>
 										<td></td>
