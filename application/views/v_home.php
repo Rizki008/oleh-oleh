3c90d5db2c7cffd7bd654a112ef7793198af822e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-09-15 15:31:24 +0700

    menghapus data nama kategori

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -56,11 +56,11 @@
 			<h2 class="h5 text-uppercase mb-4">Browse our categories</h2>
 		</header>
 		<div class="row">
-			<div class="col-md-4"><a class="category-item" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/jeniv.jpeg" alt="" /><strong class="category-item-title">Clothes</strong></a>
+			<div class="col-md-4"><a class="category-item" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/jeniv.jpeg" alt="" /><strong class="category-item-title"></strong></a>
 			</div>
-			<div class="col-md-4"><a class="category-item mb-4" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/ketan.jpg" alt="" /><strong class="category-item-title">Shoes</strong></a><a class="category-item" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/ahh.jpg" alt="" /><strong class="category-item-title">Watches</strong></a>
+			<div class="col-md-4"><a class="category-item mb-4" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/ketan.jpg" alt="" /><strong class="category-item-title"></strong></a><a class="category-item" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/ahh.jpg" alt="" /><strong class="category-item-title"></strong></a>
 			</div>
-			<div class="col-md-4"><a class="category-item" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/emping.jpeg" alt="" /><strong class="category-item-title">Electronics</strong></a>
+			<div class="col-md-4"><a class="category-item" href="#"><img class="img-fluid" src="<?= base_url() ?>distribution/img/emping.jpeg" alt="" /><strong class="category-item-title"></strong></a>
 			</div>
 		</div>
 	</section>
