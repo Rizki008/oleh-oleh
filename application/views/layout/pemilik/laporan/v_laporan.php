b2d3db7e6d3814c46bee9f6772f49ff87ccfee93
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-09-27 14:06:36 +0700

    ubah data laporan

--- application/views/layout/pemilik/laporan/v_laporan.php
+++ application/views/layout/pemilik/laporan/v_laporan.php
@@ -10,7 +10,7 @@
 			</nav>
 		</div>
 		<div class="row">
-			<div class="col-lg-12 grid-margin stretch-card">
+			<div class="col-lg-4 grid-margin stretch-card">
 				<div class="card">
 					<div class="card-body">
 						<?php
@@ -45,6 +45,88 @@
 								</div>
 							</div>
 							<div class="col-sm-4">
+								<div class="form-group">
+									<label>Tahun</label>
+									<select name="tahun" class="form-control">
+										<?php
+										$mulai = date('Y') - 1;
+										for ($i = $mulai; $i < $mulai + 10; $i++) {
+											$sel = $i == date('Y') ? 'selected="selected"' : '';
+											echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+										}
+										?>
+									</select>
+								</div>
+							</div>
+							<div class="col-sm-12">
+								<div class="form-group">
+									<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cetak Laporan</button>
+								</div>
+							</div>
+						</div>
+						<?php
+						echo form_close() ?>
+					</div>
+					<!-- /.card-body -->
+				</div>
+				<!-- /.card -->
+			</div>
+
+			<div class="col-lg-4 grid-margin stretch-card">
+				<div class="card">
+					<div class="card-body">
+						<?php
+						echo form_open('laporan/lap_bulanan') ?>
+						<div class="row">
+							<div class="col-sm-6">
+								<div class="form-group">
+									<label>Bulan</label>
+									<select name="bulan" class="form-control">
+										<?php
+										$mulai = 1;
+										for ($i = $mulai; $i < $mulai + 12; $i++) {
+											$sel = $i == date('Y') ? 'selected="selected"' : '';
+											echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+										}
+										?>
+									</select>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="form-group">
+									<label>Tahun</label>
+									<select name="tahun" class="form-control">
+										<?php
+										$mulai = date('Y') - 1;
+										for ($i = $mulai; $i < $mulai + 10; $i++) {
+											$sel = $i == date('Y') ? 'selected="selected"' : '';
+											echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+										}
+										?>
+									</select>
+								</div>
+							</div>
+							<div class="col-sm-12">
+								<div class="form-group">
+									<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cetak Laporan</button>
+								</div>
+							</div>
+						</div>
+						<?php
+						echo form_close() ?>
+					</div>
+					<!-- /.card-body -->
+				</div>
+				<!-- /.card -->
+			</div>
+
+			<div class="col-lg-4 grid-margin stretch-card">
+				<div class="card">
+					<div class="card-body">
+						<?php
+						echo form_open('laporan/lap_tahunan') ?>
+						<div class="row">
+							<div class="col-sm-12">
 								<div class="form-group">
 									<label>Tahun</label>
 									<select name="tahun" class="form-control">
